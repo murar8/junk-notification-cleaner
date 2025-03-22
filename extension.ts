e720e5854254e27363c7cc502775053df0d15285
Lorenzo Murarotto <murar8@proton.me>
2025-03-22 09:24:28 +0100

    refactor(extension): Update import statements and improve string formatting in window and source label functions

--- extension.ts
+++ extension.ts
@@ -1,7 +1,8 @@
-import * as Main from "resource:///org/gnome/shell/ui/main.js";
-import Gio from "gi://Gio";
-import Meta from "gi://Meta";
-import { Source } from "resource:///org/gnome/shell/ui/messageTray.js";
+import type Gio from "gi://Gio";
+import type Meta from "gi://Meta";
+import type { Source } from "resource:///org/gnome/shell/ui/messageTray.js";
+
+import Main from "resource:///org/gnome/shell/ui/main.js";
 import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 
 function getWindowLabel(window: Meta.Window) {
@@ -9,13 +10,13 @@ function getWindowLabel(window: Meta.Window) {
   const wmClass = window.wmClass ?? "<empty>";
   const gtkAppId = window.gtkApplicationId ?? "<empty>";
   const sandboxedAppId = window.get_sandboxed_app_id() ?? "<empty>";
-  return `Window(Title: ${title}, WMClass: ${wmClass}, GTKAppId: ${gtkAppId}, SandboxedAppId: ${sandboxedAppId})`;
+  return `Window(Title: '${title}', WMClass: '${wmClass}', GTKAppId: '${gtkAppId}', SandboxedAppId: '${sandboxedAppId}')`;
 }
 
 function getSourceLabel(source: Source) {
   const title = source.title ?? "<empty>";
   const icon = source.icon?.to_string() ?? "<empty>";
-  return `Source(Title: ${title}, Icon: ${icon})`;
+  return `Source(Title: '${title}', Icon: '${icon}')`;
 }
 
 function isMatch(window: Meta.Window, source: Source) {
@@ -23,18 +24,22 @@ function isMatch(window: Meta.Window, source: Source) {
   if (source.icon) {
     const icon = source.icon.to_string();
     if (
-      // For Ghostty DEB (and maybe other GTK apps) icon is the app id (com.mitchellh.ghostty)
+      // For Ghostty deb (and maybe other GTK apps) source icon is the same as the GTK app id
+      // i.e. com.mitchellh.ghostty
       icon === window.gtkApplicationId ||
-      // For Slack Flatpak (and maybe other flatpaks) icon is the app id (com.slack.Slack)
+      // For Slack Flatpak (and maybe other flatpaks and snaps) source icon is the same as the app id
+      // i.e. com.slack.Slack
       icon === sandboxedAppId ||
-      // For firefox DEB icon is the window manager class (firefox)
+      // For Firefox deb source icon is the same as the window manager class
+      // i.e. firefox
       icon === window.wmClass
     ) {
       return true;
     }
   }
   return (
-    // For Proton Mail Bridge the title is the same as the window title.
+    // For Proton Mail Bridge source title is the same as the window title.
+    // i.e. Proton Mail Bridge
     source.title === window.title
   );
 }
