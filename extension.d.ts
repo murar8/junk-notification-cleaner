3aca73ff652d360aab41eca7a0d9092b510a7f8f
Lorenzo Murarotto <murar8@proton.me>
2025-05-31 14:01:10 +0200

    set up release

--- extension.d.ts
+++
@@ -1,9 +0,0 @@
-import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
-export default class JunkNotificationCleaner extends Extension {
-    private focusListenerId;
-    private closeListenerId;
-    private settings;
-    private clearNotificationsForApp;
-    enable(): void;
-    disable(): void;
-}
--- prefs.d.ts
+++
@@ -1,8 +0,0 @@
-import Adw from "gi://Adw";
-import Gio from "gi://Gio";
-import Gtk from "gi://Gtk";
-import { ExtensionPreferences } from "resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js";
-export default class JunkNotificationCleanerPreferences extends ExtensionPreferences {
-    fillPreferencesWindow(window: Adw.PreferencesWindow): Promise<void>;
-    addExcludedAppRow(app: string, listBox: Gtk.ListBox, settings: Gio.Settings): void;
-}
